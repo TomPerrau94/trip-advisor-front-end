4bcfbb6263ca20b3b43f6d8171dcc798cf91b7d1
Tom Perrau <tomperrau@macbook-pro-tom-1.home>
2020-07-18 19:16:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Incomplete TripAdivsor replica
+
+## Front-End
+
+Practiced responsiveness on most of the elements and sections of the page. Try them yourself, but don't go below 768px breakpoint !
+
+## Back-End
+
+Linked my page to a server and a database so that you can use the "Sign-Up" ("S'inscrire") button. Haven't figured out a way to display an error when user email already exists in database, but other than that it works just fine !
---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+const $ = document;
+
+$.addEventListener("DOMContentLoaded", () => {
+  console.log("Document loaded");
+
+  const modal = $.getElementById("sign-up-modal");
+
+  const button = $.getElementById("sign-up-button");
+
+  const closeButton = $.getElementById("sign-up-modal-close");
+
+  const form = $.getElementById("sign-up-form");
+
+  const returnButton = $.getElementById("return-to-website");
+
+  const signupSuccess = $.getElementById("sign-up-success");
+
+  // const signupError = $.getElementById("sign-up-error");
+
+  button.onclick = function () {
+    modal.style.display = "flex";
+    $.body.style.overflow = "hidden";
+  };
+
+  closeButton.onclick = function () {
+    modal.style.display = "none";
+    $.body.style.overflow = null;
+  };
+
+  returnButton.onclick = function () {
+    modal.style.display = "none";
+    $.body.style.overflow = null;
+    $.location.reload(true);
+  };
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  $.querySelector("#sign-up-form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const data = {
+      firstname: $.querySelector("#firstname").value,
+      lastname: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      password: $.querySelector("#password").value,
+      description: $.querySelector("#description").value,
+    };
+
+    // console.log(data);
+    const response = await axios.post("http://localhost:3000/sign-up", data);
+    // console.log(response);
+
+    if (response.request.status === 200) {
+      form.style.display = "none";
+      signupSuccess.style.display = "flex";
+    }
+    // else {
+    //   form.style.display = "none";
+    //   signupError.style.display = "flex";
+    // }
+  });
+});
